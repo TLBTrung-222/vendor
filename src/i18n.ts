8c2ca639228286801f7a9173882e7b9e2b10f353
Lâm Ngọc <lamthanhngoc713@gmail.com>
2025-11-11 09:34:44 +0700

    Refactor i18n configuration for improved language detection and support

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,22 +1,27 @@
 // src/i18n.ts
-import i18n from 'i18next';
-import { initReactI18next } from 'react-i18next';
-import HttpBackend from 'i18next-http-backend';
-import LanguageDetector from 'i18next-browser-languagedetector';
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+import HttpBackend from "i18next-http-backend";
+import LanguageDetector from "i18next-browser-languagedetector";
 
 i18n
-    .use(HttpBackend)
-    .use(LanguageDetector)
-    .use(initReactI18next)
-    .init({
-        fallbackLng: 'en',
-        debug: false,
-        interpolation: {
-            escapeValue: false,
-        },
-        backend: {
-            loadPath: '/locales/{{lng}}.json',
-        },
-    });
+  .use(HttpBackend)
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    fallbackLng: "de",
+    debug: false,
+    interpolation: {
+      escapeValue: false,
+    },
+    backend: {
+      loadPath: "/locales/{{lng}}.json",
+    },
+    supportedLngs: ["de", "en"],
+    detection: {
+      order: ["localStorage"],
+      caches: ["localStorage"],
+    },
+  });
 
 export default i18n;
